dcfa0cf5f539740c0f805976dc43bc133e2e0e96
EnesSastim <sastimenes@gmail.com>
2022-05-22 14:57:07 +0000

    core: include proton sepolicy

--- core/config.mk
+++ core/config.mk
@@ -1161,6 +1161,8 @@ dont_bother_goals := out \
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
+include device/proton/sepolicy/sepolicy.mk
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
